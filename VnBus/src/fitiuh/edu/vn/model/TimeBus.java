32db41697d26d450f3986994e5f32a877effd34e
LeDangIUH <lcongdang@gmail.com>
2014-05-08 21:58:21 +0700

    Comple add database to data system

---
+++ VnBus/src/fitiuh/edu/vn/model/TimeBus.java
@@ -0,0 +1,26 @@
+package fitiuh.edu.vn.model;
+
+public class TimeBus {
+	private String place;
+	private String timeStart;
+	private String timeEnd;
+	
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getTimeStart() {
+		return timeStart;
+	}
+	public void setTimeStart(String timeStart) {
+		this.timeStart = timeStart;
+	}
+	public String getTimeEnd() {
+		return timeEnd;
+	}
+	public void setTimeEnd(String timeEnd) {
+		this.timeEnd = timeEnd;
+	}
+}
--- VnBus/src/fitiuh/edu/vn/vnbus/FN0001.java
+++ VnBus/src/fitiuh/edu/vn/vnbus/FN0001.java
@@ -1,5 +1,36 @@
 package fitiuh.edu.vn.vnbus;
+import fitiuh.edu.vn.base.BaseDatabaseActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 
-public class FN0001 {
-
+public class FN0001 extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fn0001);
+		
+		BaseDatabaseActivity baseDatabaseActivity = new BaseDatabaseActivity(getApplicationContext());
+		try {
+			baseDatabaseActivity.createDataBase();
+		} catch (Exception e) {
+			throw new Error("Unable to create database");
+		}
+		try {
+			baseDatabaseActivity.openDataBase();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		Log.w("ok", "-----OK");		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
